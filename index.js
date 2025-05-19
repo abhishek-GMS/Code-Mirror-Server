688c6efadc9d35a1ba1cde33e4335729f494edd4
Abhishek Salokhe <Abhishek Salokhe@gms.com>
2025-05-19 15:04:19 +0530

    Frontend URL changed in CROS

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(express.json());
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://fast-job-ai-code-editor.netlify.app/",
     methods: ["GET", "POST"],
   },
 });
